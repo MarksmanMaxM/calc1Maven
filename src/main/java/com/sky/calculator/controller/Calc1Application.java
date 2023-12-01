a33b9193e86ab61e4a1ffe7f65209efc83560a58
marksman.max <marksman.max@yandex.ru>
2023-12-01 13:26:24 +0300

    ver.2

--- src/main/java/com/sky/calculator/controller/Calc1Application.java
+++ src/main/java/com/sky/calculator/controller/Calc1Application.java
@@ -1,4 +1,4 @@
-package com.calculator.calc1;
+package com.sky.calculator.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
